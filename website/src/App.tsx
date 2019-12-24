d7e1e968ec259b36e57260c79c4e578f4dd9ca52
Brandon Fallin <bmfallin@gmail.com>
2019-12-23 21:49:05 -0500

    setup hello world

--- website/src/App.tsx
+++ website/src/App.tsx
@@ -1,26 +1,11 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { ReactElement } from 'react';
+import 'primereact/resources/primereact.min.css'
+import 'primereact/resources/themes/nova-light/theme.css'
 import './App.css';
 
-const App: React.FC = () => {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+export class App extends React.Component {
 
-export default App;
+	public render(): ReactElement {
+		return <div>Hello World!</div>
+	}
+}
