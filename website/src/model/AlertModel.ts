b13c954c3d584f4bca5e5e1ccce64d96382be9ec
Brandon Fallin <bmfallin@gmail.com>
2020-03-14 14:13:01 -0400

    Updated alerting system

--- website/src/model/AlertModel.ts
+++ website/src/model/AlertModel.ts
@@ -1,12 +1,12 @@
 export default class AlertModel {
     public readonly title: string;
-    public readonly description: string;
+    public readonly content: string;
     public readonly expirationDate: Date;
     public dismissed: boolean = false;
 
-    constructor(title: string, description: string, expirationDate: Date){
+    constructor(title: string, content: string, expirationDate: Date){
         this.title = title;
-        this.description = description;
+        this.content = content;
         this.expirationDate = expirationDate;
     }
 }
